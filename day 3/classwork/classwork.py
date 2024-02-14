53fa8cdf2d352bec0e3360e74766ddb69730b4ed
akageo <akakiasixarulidze@gmail.com>
2024-02-15 00:01:18 +0400

    commit

--- day 3/classwork/classwork.py
+++ day 3/classwork/classwork.py
@@ -7,10 +7,10 @@ print =(last_name)
 
 print(name, last_name,)
 my_age = 15
+
 print(type(my_age))
 print("my_name is" +  str(my_age))
 item = "bike"
 #item არის ცვლადის სახელი
 # = არის მინიჭების ოპერატორი
 #bike არის ცვლადის მნიშვნელობა
-
