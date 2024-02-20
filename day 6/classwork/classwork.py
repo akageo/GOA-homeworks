b5857e11f82ece39d1ea0d7dd522561c1e68e709
akageo <akakiasixarulidze@gmail.com>
2024-02-21 00:01:22 +0400

    commit

---
+++ day 6/classwork/classwork.py
@@ -0,0 +1,8 @@
+#person name
+name = input("type your first name? : ")
+#person last_name
+last_name = input("type your last name? : ")
+#person age
+age = input ("choose your age? : ")
+
+print("Your name is" + name +"you are "+ age + "years old" + " your last_name is" + last_name,age)
