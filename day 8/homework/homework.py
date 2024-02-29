f02b239fe8ccf366f9a8ede7c05e9c040c48b7e0
akageo <akakiasixarulidze@gmail.com>
2024-02-29 13:37:51 +0400

    commit

--- day 8/homework/homework.py
+++ day 8/homework/homework.py
@@ -3,16 +3,16 @@
 required_weight = 50
 required_height = 170
 
-weight = int(input("enter your current weight"))
-height = int(input("enter your height"))
+weight = int(input("enter your current weight: "))
+height = int(input("enter your height: "))
 
 print(weight == required_weight and height == required_height)
 
 required_weight_one = 50
 required_height_one = 170
 
-weight_one = int(input("enter your current weight"))
-height_one = int(input("enter your height"))
+weight_one = int(input("enter your current weight: "))
+height_one = int(input("enter your height: "))
 
 print(weight >= required_weight_one and height <= required_height_one)
 
@@ -21,7 +21,7 @@ print(weight >= required_weight_one and height <= required_height_one)
 requaired_pushups = 100
 requaired_squats = 50
 
-pushups = int(input)("how many pushups have u done")
-squats = int(input)("how many squats have u done")
+pushups = int(input("how many pushups have u done: "))
+squats = int(input("how many squats have u done: "))
 
 print(pushups == requaired_pushups and squats == requaired_squats)
