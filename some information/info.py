e31566d81c5431de04e6e1cb9576f9c00cfa90b0
akageo <akakiasixarulidze@gmail.com>
2024-03-11 06:59:35 +0400

    commit

--- some information/info.py
+++ some information/info.py
@@ -37,6 +37,8 @@ print ("5 == 3")
 
 # f"" არის f სთრინგი რომელიც გვიმარტივებს ჩვენ ცვლადებზე მუშაობს, როდესაც მათი გამოტანის - output-ის დრო არის. ჩვენ შეგვიძლია, რომ დავწეროთ f, შემდეგ ჩვეულებრივად ბრჭყალები, რათა ტექსტი შევიტანოთ ცვლადებთან ერთად. ბრჭყალებში როდესაც დაგვჭირდება ცვლადის შემოტანა გავხსნით დაკლაკნილ ფრჩხილებს {} და მას გადავცემთ ცვლადის სახელს.
 
+--------------------------------------------------------------------------------------------------------------------------------------------
+
 num5 = int(input("enter your first number: "))
 num6 = int(input("enter your second number: "))
 sum = 0
@@ -49,3 +51,54 @@ elif num6 > num5:
 else:
     print(f"{num5}={num6}")
 print(sum)
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#         0    1          2
+#                        0    1
+_tuple = (1 , 50, ['daviti', 'guja'])
+#        1      2
+_arr = ['დათვი', 'თეთრი დათვი', 'შავი დათვი']
+
+_arr[0] = 'ზებრა'
+
+_tuple[2][1] = 'lamborgini'
+
+print(_tuple)
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+#The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
+a = 77
+b = 77
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+#The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+
+#ExampleGet your own Python Server
+#Print all numbers from 0 to 5, and print a message when the loop has ended:
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+#Booleans represent one of two values: True or False.
+
+In programming you often need to know if an expression is True or False.
+You can evaluate any expression in Python, and get one of two answers, True or False.
+When you compare two values, the expression is evaluated and Python returns the Boolean answer:
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print(10 != 5) არ უდრის
